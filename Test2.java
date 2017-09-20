0a445d92260d73f2883b92b6c6888e3a2d7a5fe7
tutorial-kk <kb09@netsgo.com>
2017-09-21 03:45:01 +0900

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test2 {
+	
+	public void test2() {
+		// TODO Auto-generated method stub
+		System.out.println("Test2 !!!!!!");
+
+	}
+
+}
---
+++ git_test.java
@@ -0,0 +1,24 @@
+package test;
+
+public class git_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git-Hub TEST !!!!!!!!!!!!!!");
+		System.out.println("===========================");
+		System.out.println("Branch TEST !!!!!!!!!!!!!!");
+
+		
+		System.out.println("Branch TEST 재수정 !!!!!!!!!!!!!!");
+		
+		Test2 t2= new Test2();
+		//Test2.test2();
+		t2.test2();
+		t2.test2();
+
+		System.out.println("Branch 출력추가 !!!!!!!!!!!!!!");
+		
+	}
+
+}
