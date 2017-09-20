d766f359d25a518b43ef5074f98bce114ca3e0c2
kk(kibera) <kk(kibera)@kkkaberi-PC>
2017-09-21 02:07:07 +0900

    브랜치 출력 추가

--- test/src/test/git_test.java
+++ test/src/test/git_test.java
@@ -8,6 +8,8 @@ public class git_test {
 		System.out.println("Git-Hub TEST !!!!!!!!!!!!!!");
 		System.out.println("===========================");
 		System.out.println("Branch TEST !!!!!!!!!!!!!!");
+		System.out.println("Branch 출력추가 !!!!!!!!!!!!!!");
+		
 	}
 
 }
